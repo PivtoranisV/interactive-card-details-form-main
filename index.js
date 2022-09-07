3f93570bcdbe55e5166a6582ba9d1ff3305e9f8d
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-09-07 07:29:35 +0400

    Added function for card number

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const inputName = document.querySelector("#inputName");
 const nameOnTheCard = document.querySelector(".card-img__front__text__name");
+const inputNumber = document.querySelector("#inputNumber");
+const numberOnTheCard = document.querySelector(
+  ".card-img__front__text__number"
+);
 
 inputName.addEventListener("keyup", () => {
   nameOnTheCard.innerHTML = inputName.value;
 });
+
+inputNumber.addEventListener("keyup", displayNumber);
+
+function displayNumber() {
+  let cardNumber = inputNumber.value;
+  let format = [...cardNumber].map((el, index) => {
+    if ((index + 1) % 4 === 0) {
+      el = el + " ";
+    }
+    return el;
+  });
+  numberOnTheCard.innerHTML = format.join("");
+}
