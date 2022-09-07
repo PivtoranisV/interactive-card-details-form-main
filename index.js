dd9ec720bb68f1eb571628a85fde63d3558198c9
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-09-07 07:54:57 +0400

    Added exp.date function

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
+//variable declaration
 const inputName = document.querySelector("#inputName");
 const nameOnTheCard = document.querySelector(".card-img__front__text__name");
 const inputNumber = document.querySelector("#inputNumber");
 const numberOnTheCard = document.querySelector(
   ".card-img__front__text__number"
 );
+const inputMonth = document.querySelector("#inputExpDate");
+const monthOnTheCard = document.querySelector(".mm");
+const inputYear = document.querySelector("#inputDate");
+const yearOnTheCard = document.querySelector(".yy");
 
+// Event Listeners
 inputName.addEventListener("keyup", () => {
   nameOnTheCard.innerHTML = inputName.value;
 });
 
 inputNumber.addEventListener("keyup", displayNumber);
+
+inputMonth.addEventListener("keyup", () => {
+  monthOnTheCard.innerHTML = inputMonth.value;
+});
+
+inputYear.addEventListener("keyup", () => {
+  yearOnTheCard.innerHTML = inputYear.value;
+});
+
+// Helper function
 
 function displayNumber() {
   let cardNumber = inputNumber.value;
